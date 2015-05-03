036c7e351ece560fec5895918cf280a6238f4e69
Liciax <aboubouille@gmail.com>
2015-05-03 17:42:08 +0200

    ajout d'un programme en C++  qui calcule si une année est bissextile ou non

---
+++ bissextile.cpp
@@ -0,0 +1,31 @@
+/*-----------------------------------------------------
+ OpenClassrooms : Cours Git & GitHub
+ Git & GitHub - Partie 2 Activité
+ auteur : Liciax
+ 
+ Programme qui teste si une année est bissextile ou non
+-------------------------------------------------------*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//variables
+	int annee;
+	
+	cout << "Saisir une année : " << endl;
+	cin >> annee;
+	
+	if ((annee % 400 == 0)||((annee % 4 == 0)&&(annee % 100 != 0))) {
+		cout << "L'année " << annee << " est une année bissextile." << endl;
+	}
+	else
+	{
+		cout << "L'année " << annee << " n'est pas une année bissextile." << endl;
+	}
+	return 0;
+} //main
